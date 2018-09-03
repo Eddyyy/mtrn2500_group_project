fc1c183122c5f5a4689c3954cd51e37a23558e9b
Eddyyy <edward.thomson9@gmail.com>
2018-09-04 04:02:16 +1000

    Finished vehicle properly

--- Project3/Project3/MyVehicle.cpp
+++ Project3/Project3/MyVehicle.cpp
@@ -1,6 +1,9 @@
 #include "MyVehicle.hpp"
 #include "Shape.hpp"
 #include "RectPrism.hpp"
+#include "TriPrism.hpp"
+#include "TrapPrism.hpp"
+#include "Cylinder.hpp"
 
 
 #ifdef __APPLE__
@@ -19,9 +22,23 @@
 #endif
 
 MyVehicle::MyVehicle() : Vehicle() {
-	Shape* body = new RectPrism(0, 0, 0, 3, 2, 2);
+	Shape* body = new RectPrism(0.0, 0.0, 0.0, 3.0, 2.0, 2.0);
 	body->setColor(0, 1, 1);
 	MyVehicle::addShape(body);
+
+	TriPrism * driver = new TriPrism(0.75, 2.0, 0.0, 0.5, 0.5, 45, 1.0);
+	driver->setColor(1, 0, 0);
+	driver->draw();
+	MyVehicle::addShape(driver);
+
+	TrapPrism * passenger = new TrapPrism(-0.75, 2.0, 0.0, 1.5, 1.0, 1.0, 2.0, 0.25);
+	passenger->setColor(0, 0, 1);
+	passenger->draw();
+	MyVehicle::addShape(passenger);
+
+	Cylinder * testCylinder = new Cylinder(1.5, 0.5, 0.0, 90, 0.5, 1.0);
+	testCylinder->setColor(1, 1, 0);
+	testCylinder->draw();
 }
 
 void MyVehicle::draw() {
